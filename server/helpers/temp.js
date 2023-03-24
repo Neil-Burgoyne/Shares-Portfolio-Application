792eb1b1f4371c06be883fa186809adeea624fc4
TimoHenderson <tjhenderson2@gmail.com>
2023-03-24 15:04:33 +0000

    temp data added

---
+++ server/helpers/temp.js
@@ -0,0 +1,21 @@
+
+
+const dataforUi = {
+    "name": "Millicent Moneybags",
+    "shareValues": [
+        {
+            "stockSymbol": "AAPL",
+            "numshares": 120,
+            "averagePricePaid": 35,
+            "currentMarketValue": 125
+        },
+        {
+            "stockSymbol": "IBM",
+            "numshares": 120,
+            "averagePricePaid": 35,
+            "currentMarketValue": 125
+        }
+    ]
+
+}
+
