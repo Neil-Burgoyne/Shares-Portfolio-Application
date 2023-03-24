e5e77fb83f2aa7ccc460acac3f4dca90ca730f1f
Neil-Burgoyne <neil-burgoyne@hotmail.com>
2023-03-24 11:31:11 +0000

    Update create_router.js

---
+++ server/helpers/create_router.js
@@ -0,0 +1,84 @@
+const express = require("express")
+const ObjectID = require("mongodb").ObjectID
+
+const createRouter = function (collection) {
+    const router = express.Router()
+
+
+    // Index Route
+    router.get('/', (req, res) => {
+        collection
+            .find()
+            .toArray()
+            .then((docs) => res.json(docs))
+            .catch((err) => {
+                console.error(err)
+                res.status(500)
+                res.json({ status: 500, error: err })
+            })
+    })
+
+    // Show Route
+    router.get('/:id', (req, res) => {
+        const id = req.params.id
+        collection
+            .findOne({ _id: ObjectID(id) })
+            .then((docs) => res.json(docs))
+            .catch((err) => {
+                console.error(err)
+                res.status(500)
+                res.json({ status: 500, error: err })
+            })
+    })
+
+    // Create Route
+    router.post('/', (req, res) => {
+        const newData = req.body
+        collection
+            .insertOne(newData)
+            .then(() => collection.find().toArray())
+            .then((docs) => res.json(docs))
+            .catch((err) => {
+                console.error(err)
+                res.status(500)
+                res.json({ status: 500, error: err })
+            })
+    })
+
+    // Destroy Route
+    router.put('/:id', (req, res) => {
+        const id = req.params.id
+        const updatedData = req.body
+        collection
+            .updateOne(
+                { _id: ObjectID(id) },
+                { $set: updatedData }
+            )
+            .then(() => collection.find().toArray())
+            .then((docs) => res.json(docs))
+            .catch((err) => {
+                console.error(err);
+                res.status(500);
+                res.json({ status: 500, error: err })
+            })
+    })
+
+    // Update Route
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id
+        collection
+            .deleteOne({ _id: ObjectID(id) })
+            .then(() => collection.find().toArray())
+            .then((docs) => res.json(docs))
+            .catch((err) => {
+                console.error(err)
+                res.status(500)
+                res.json({ status: 500, error: err })
+            })
+    })
+
+    return router
+
+}
+
+module.exports = createRouter
