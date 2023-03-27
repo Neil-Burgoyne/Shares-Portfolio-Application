9758cef45bf7dd78fffa43b80fecc74203bcc7da
TimoHenderson <tjhenderson2@gmail.com>
2023-03-27 11:30:36 +0100

    commented out test useffec

--- client/src/components/ApiTest.js
+++ client/src/components/ApiTest.js
@@ -3,34 +3,34 @@ import { getStocks, getStock } from '../api_services/StocksService';
 import { getUsers, getUser } from '../api_services/UsersService';
 
 const ApiTest = () => {
-    const [allStocks, setAllStocks] = useState([]);
-    const [stock, setStock] = useState({});
-    const [allUsers, setUsers] = useState([]);
-    const [user, setUser] = useState({});
+    // const [allStocks, setAllStocks] = useState([]);
+    // const [stock, setStock] = useState({});
+    // const [allUsers, setUsers] = useState([]);
+    // const [user, setUser] = useState({});
 
-    useEffect(() => {
-        const fetchStocks = async () => {
-            const stocks = await getStocks()
-            setAllStocks(stocks);
-        }
-        fetchStocks();
-    }, [])
+    // useEffect(() => {
+    //     const fetchStocks = async () => {
+    //         const stocks = await getStocks()
+    //         setAllStocks(stocks);
+    //     }
+    //     fetchStocks();
+    // }, [])
 
-    useEffect(() => {
-        const fetchStock = async (symbol) => {
-            const stock = await getStock(symbol)
-            setStock(stock);
-        }
-        fetchStock("AAPL");
-    }, [])
+    // useEffect(() => {
+    //     const fetchStock = async (symbol) => {
+    //         const stock = await getStock(symbol)
+    //         setStock(stock);
+    //     }
+    //     fetchStock("AAPL");
+    // }, [])
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const users = await getUsers()
-            setUsers(users);
-        }
-        fetchUsers();
-    }, [])
+    // useEffect(() => {
+    //     const fetchUsers = async () => {
+    //         const users = await getUsers()
+    //         setUsers(users);
+    //     }
+    //     fetchUsers();
+    // }, [])
 
     // useEffect(() => {
     //     const fetchUser = async (id) => {
