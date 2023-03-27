53eb15212367656ff28433346985c42936c2f106
TimoHenderson <tjhenderson2@gmail.com>
2023-03-27 11:28:49 +0100

    aaded user service

--- client/src/components/ApiTest.js
+++ client/src/components/ApiTest.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { getStocks, getStock } from '../api_services/StocksService';
+import { getUsers, getUser } from '../api_services/UsersService';
 
 const ApiTest = () => {
     const [allStocks, setAllStocks] = useState([]);
     const [stock, setStock] = useState({});
+    const [allUsers, setUsers] = useState([]);
+    const [user, setUser] = useState({});
 
     useEffect(() => {
         const fetchStocks = async () => {
@@ -20,6 +23,23 @@ const ApiTest = () => {
         }
         fetchStock("AAPL");
     }, [])
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const users = await getUsers()
+            setUsers(users);
+        }
+        fetchUsers();
+    }, [])
+
+    useEffect(() => {
+        const fetchUser = async (id) => {
+            const user = await getUser(id)
+            setUser(user);
+        }
+        fetchUser("642167b19ac0f323d0882a48");
+    }, [])
+
 
     return (<h1>ApiTest</h1>);
 }
