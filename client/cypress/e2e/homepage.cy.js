68c0e7f6a718915375e16e4eb03f6828356f2736
Neil-Burgoyne <neil-burgoyne@hotmail.com>
2023-03-28 14:22:20 +0100

    Express Testing

---
+++ client/cypress/e2e/homepage.cy.js
@@ -0,0 +1,11 @@
+describe("Homepage Loads", () => {
+    it("loads the homepage correctly", () => {
+        cy.visit("http://localhost:3000")
+    })
+
+    // it("can add shares to the user profile", () => {
+    //     cy.visit("/")
+    // })
+
+
+})
