520f2156f5d71275581607b8ae370762d5f53196
TimoHenderson <tjhenderson2@gmail.com>
2023-03-28 18:45:04 +0100

    limited max sell shares

--- client/src/components/PortfolioTableRow.js
+++ client/src/components/PortfolioTableRow.js
@@ -13,110 +13,111 @@ import PageviewIcon from '@mui/icons-material/Pageview';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 
 
-const PortfolioTableRow = ({stock, sellShares, deleteShare, editShare}) => {
+const PortfolioTableRow = ({ stock, sellShares, deleteShare, editShare }) => {
     const [clicked, setClicked] = useState(false);
     const [editClicked, setEditClicked] = useState(false);
     const [shareInput, setShareInput] = useState();
     const [editForm, setEditForm] = useState(stock);
 
-    const editClick = ()=>{
+    const editClick = () => {
         setClicked(false)
         setEditClicked(!editClicked);
     }
 
-    const sellClick = ()=>{
+    const sellClick = () => {
         setEditClicked(false)
         setClicked(!clicked)
     }
 
-    const onChange = (e)=>{
+    const onChange = (e) => {
         setShareInput(e.target.value)
     }
 
-    const sell =()=>{
-        if(shareInput <= stock.numShares){
+    const sell = () => {
+        if (shareInput <= stock.numShares) {
             sellShares(Number(shareInput), stock)
             sellClick()
-        } else{
+        } else {
             return null
         }
     }
 
-    const deleteEntry = ()=>{
+    const deleteEntry = () => {
         setEditClicked(false)
         deleteShare(stock)
     }
 
-    const answer = ((stock.currentMarketValue / stock.averagePricePaid)*100)
+    const answer = ((stock.currentMarketValue / stock.averagePricePaid) * 100)
 
-    const editChange = (e)=>{
+    const editChange = (e) => {
         editForm[e.target.id] = Number(e.target.value)
         setEditForm(editForm)
     }
 
-    const submitEditEntry = ()=>{
+    const submitEditEntry = () => {
         editShare(editForm, stock)
         setEditForm(stock)
         setEditClicked(!editClicked);
     }
 
 
-    return( 
-    <>
-        <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-            <TableCell component="th" scope="row">{stock.symbol}</TableCell>
-            <TableCell>{stock.numShares}</TableCell>
-            <TableCell>£{stock.averagePricePaid}</TableCell>
-            <TableCell>£{stock.currentMarketValue}</TableCell>
-            {answer >= 100 ? <TableCell>&#8593;{(answer-100).toFixed(2)}%</TableCell> : <TableCell>&#8595;{(100-answer).toFixed(2)}%</TableCell>}
-            <TableCell><Link to="/view"><PageviewIcon/></Link></TableCell>
-            <TableCell onClick={editClick}><SettingsIcon/></TableCell>
-            <TableCell onClick={sellClick}><AttachMoneyIcon/></TableCell>
-        </TableRow>
-        <TableRow>
-        <TableCell className="cell" style={{ paddingBottom: 0, paddingTop: 0}} colSpan={8}>
-        <Collapse in={clicked} timeout="auto" unmountOnExit>
-                <TableHead>
-                <TableRow>
-                    <TableCell>Sell Shares</TableCell>
-                </TableRow>
+    return (
+        <>
+            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+                <TableCell component="th" scope="row">{stock.symbol}</TableCell>
+                <TableCell>{stock.numShares}</TableCell>
+                <TableCell>£{stock.averagePricePaid}</TableCell>
+                <TableCell>£{stock.currentMarketValue}</TableCell>
+                {answer >= 100 ? <TableCell>&#8593;{(answer - 100).toFixed(2)}%</TableCell> : <TableCell>&#8595;{(100 - answer).toFixed(2)}%</TableCell>}
+                <TableCell><Link to="/view"><PageviewIcon /></Link></TableCell>
+                <TableCell onClick={editClick}><SettingsIcon /></TableCell>
+                <TableCell onClick={sellClick}><AttachMoneyIcon /></TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell className="cell" style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={8}>
+                    <Collapse in={clicked} timeout="auto" unmountOnExit>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Sell Shares</TableCell>
+                            </TableRow>
 
-                </TableHead>
-                <TableRow>        
-                    <TableCell><input id="input" type='number' onChange={onChange} placeholder='Number to sell'></input></TableCell>
-                    <TableCell><button onClick={sell}>Sell Shares</button></TableCell>
-                    <TableCell><button onClick={sellClick}>Cancel</button></TableCell>
-                </TableRow>
-        </Collapse>
-        </TableCell>
-        </TableRow>
-        
-        <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0, margin:0}} colSpan={8}>
-            <Collapse in={editClicked} timeout="auto" unmountOnExit>
-                <TableHead>
-                    <TableCell>Share</TableCell>
-                    <TableCell>Shares Held</TableCell>
-                    <TableCell>Average Value</TableCell>
-                    <TableCell>Current Value</TableCell>
-                </TableHead>
-                <TableBody>
-                <TableRow>        
-                    <TableCell><input type='text' readOnly value={stock.symbol}></input></TableCell>
-                    <TableCell><input onChange={editChange} id='numShares' type='number' defaultValue={stock.numShares}></input></TableCell>
-                    <TableCell><input onChange={editChange} id='averagePricePaid' type='number' defaultValue={stock.averagePricePaid}></input></TableCell>
-                    <TableCell><input onChange={editChange} id='currentMarketValue' type='number' defaultValue={stock.currentMarketValue}></input></TableCell>
-                    <TableCell><button onClick={deleteEntry}>Delete</button></TableCell>
-                    <TableCell><button onClick={submitEditEntry}>Confirm Changes</button></TableCell>
-                    <TableCell><button onClick={editClick}>Cancel</button></TableCell>
-                </TableRow>
-                </TableBody>
-            </Collapse>
-        </TableCell>
-        </TableRow>
+                        </TableHead>
+                        <TableRow>
+                            <TableCell><input id="input" type='number' onChange={onChange} max={stock.numShares} placeholder='Number to sell'></input></TableCell>
+                            <TableCell><button onClick={sell}>Sell Shares</button></TableCell>
+                            <TableCell><button onClick={sellClick}>Cancel</button></TableCell>
+                        </TableRow>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0, margin: 0 }} colSpan={8}>
+                    <Collapse in={editClicked} timeout="auto" unmountOnExit>
+                        <TableHead>
+                            <TableCell>Share</TableCell>
+                            <TableCell>Shares Held</TableCell>
+                            <TableCell>Average Value</TableCell>
+                            <TableCell>Current Value</TableCell>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell><input type='text' readOnly value={stock.symbol}></input></TableCell>
+                                <TableCell><input onChange={editChange} id='numShares' type='number' defaultValue={stock.numShares}></input></TableCell>
+                                <TableCell><input onChange={editChange} id='averagePricePaid' type='number' defaultValue={stock.averagePricePaid}></input></TableCell>
+                                <TableCell><input onChange={editChange} id='currentMarketValue' type='number' defaultValue={stock.currentMarketValue}></input></TableCell>
+                                <TableCell><button onClick={deleteEntry}>Delete</button></TableCell>
+                                <TableCell><button onClick={submitEditEntry}>Confirm Changes</button></TableCell>
+                                <TableCell><button onClick={editClick}>Cancel</button></TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
 
 
-    </>
-)};
+        </>
+    )
+};
 
 export default PortfolioTableRow;
