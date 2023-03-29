9c37cd07d22145f9701f7f085f64bf6c0505c70f
Ross Condie <rosscondie@gmail.com>
2023-03-29 13:48:41 +0200

    added List to NewsCard component

--- client/src/components/NewsCard.js
+++ client/src/components/NewsCard.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import List from '@mui/material/List';
+import { ListItem } from '@mui/material';
 
 const NewsCard = ({ article }) => {
-  return <div>{article.headline}</div>;
+  return (
+    <List>
+      <ListItem>
+        <img
+          style={{ height: '5rem', alignContent: 'center' }}
+          src={article.image}
+          alt=""
+        />
+      </ListItem>
+      <ListItem>{article.headline}</ListItem>
+    </List>
+  );
 };
 
 export default NewsCard;
